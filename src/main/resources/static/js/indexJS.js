f482a99b08c5fccafc35e2ab0632fe7f5192e958
Harry-139 <harrydh159@outlook.com>
2026-09-08 10:00:13 +0800

    Finish building the choose bar, except for the open and close of the choose bar.

--- src/main/resources/static/js/indexJS.js
+++ src/main/resources/static/js/indexJS.js
@@ -1,14 +1,21 @@
 const categoriesSection = document.getElementById('categories');
 const trapezoidOfCategories = document.getElementById('of-categories');
+const chooseBarForCategories = document.getElementById('for-categories');
 
 function viewCategories() {
     if (trapezoidOfCategories) {
         trapezoidOfCategories.style.display = 'block';
     }
+    if (chooseBarForCategories) {
+        chooseBarForCategories.style.display = 'flex';
+    }
 }
 function hideCategories() {
     if (trapezoidOfCategories) {
         trapezoidOfCategories.style.display = 'none';
+    }
+    if (chooseBarForCategories) {
+        chooseBarForCategories.style.display = 'none';
     }
 }
 if (categoriesSection) {
@@ -18,15 +25,22 @@ if (categoriesSection) {
 
 const plansSection = document.getElementById('plans');
 const trapezoidOfPlans = document.getElementById('of-plans');
+const chooseBarForPlans = document.getElementById('for-plans');
 
 function viewPlans() {
     if (trapezoidOfPlans) {
         trapezoidOfPlans.style.display = 'block';
     }
+    if (chooseBarForPlans) {
+        chooseBarForPlans.style.display = 'flex';
+    }
 }
 function hidePlans() {
     if (trapezoidOfPlans) {
         trapezoidOfPlans.style.display = 'none';
+    }
+    if (chooseBarForPlans) {
+        chooseBarForPlans.style.display = 'none';
     }
 }
 if (plansSection) {
@@ -36,15 +50,22 @@ if (plansSection) {
 
 const eventsSection = document.getElementById('events');
 const trapezoidOfEvents = document.getElementById('of-events');
+const chooseBarForEvents = document.getElementById('for-events');
 
 function viewEvents() {
     if (trapezoidOfEvents) {
         trapezoidOfEvents.style.display = 'block';
     }
+    if (chooseBarForEvents) {
+        chooseBarForEvents.style.display = 'flex';
+    }
 }
 function hideEvents() {
     if (trapezoidOfEvents) {
         trapezoidOfEvents.style.display = 'none';
+    }
+    if (chooseBarForEvents) {
+        chooseBarForEvents.style.display = 'none';
     }
 }
 if (eventsSection) {
